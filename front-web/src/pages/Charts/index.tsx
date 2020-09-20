b620284d1d49c04684882461eeb5133d2edb25ce
Nayara Mello <nyr.mello@gmail.com>
2020-09-20 12:03:35 -0400

    Add heroku adress

--- front-web/src/pages/Charts/index.tsx
+++ front-web/src/pages/Charts/index.tsx
@@ -22,7 +22,7 @@ const inicialPieData ={
     series: []
 }
 
-const BASE_URL = 'http://localhost:8080'
+const BASE_URL = 'http://sds1-nayara.herokuapp.com'
 
 const Charts = () => {
         const [barChartData, setBarChartData] = useState<BarChartData[]>([]);
